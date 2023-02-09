90351395e871f672ce2f7f4a51d6eba1ca7e8acd
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-02-09 16:52:07 +0100

    Make use of the InputHandler in the example to print out mouse events

--- examples/rdpserver/main.cpp
+++ examples/rdpserver/main.cpp
@@ -1,6 +1,13 @@
+// SPDX-FileCopyrightText: 2023 Arjen Hiemstra <ahiemstra@heimr.nl>
+//
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+
+#include <QDebug>
 #include <QGuiApplication>
 
+#include "InputHandler.h"
 #include "Server.h"
+#include "Session.h"
 
 int main(int argc, char **argv)
 {
@@ -14,6 +21,12 @@ int main(int argc, char **argv)
     server.setPassword(QStringLiteral("test"));
     server.setTlsCertificate(QStringLiteral("server.crt"));
     server.setTlsCertificateKey(QStringLiteral("server.key"));
+
+    QObject::connect(&server, &KRdp::Server::newSession, [&server](KRdp::Session *newSession) {
+        QObject::connect(newSession->inputHandler(), &KRdp::InputHandler::inputEvent, [](QEvent *event) {
+            qDebug() << "Input event" << event;
+        });
+    });
 
     server.start();
 
