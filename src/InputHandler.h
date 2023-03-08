240f2450c98e1b63830a2fecd293905adeffa320
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-03-08 15:21:26 +0100

    Document InputHandler

--- src/InputHandler.h
+++ src/InputHandler.h
@@ -18,6 +18,11 @@ namespace KRdp
 
 class Session;
 
+/**
+ * This class processes RDP input events and converts them to Qt events.
+ *
+ * One input handler is created per session.
+ */
 class KRDP_EXPORT InputHandler : public QObject
 {
     Q_OBJECT
@@ -25,13 +30,26 @@ class KRDP_EXPORT InputHandler : public QObject
 public:
     ~InputHandler() override;
 
+    /**
+     * Emitted whenever a new input event was received from the client.
+     *
+     * \param event The input event that was received.
+     */
     Q_SIGNAL void inputEvent(QInputEvent *event);
 
 private:
     friend class Session;
+    /**
+     * InputHandler is created by Session.
+     */
     InputHandler(Session *session);
+    /**
+     * Initialize the InputHandler. Called from within Session::initialize().
+     */
     void initialize(rdpInput *input);
 
+    // FreeRDP callbacks that need to call the event handler functions in the
+    // handler.
     friend BOOL inputSynchronizeEvent(rdpInput *, uint32_t);
     friend BOOL inputMouseEvent(rdpInput *, uint16_t, uint16_t, uint16_t);
     friend BOOL inputExtendedMouseEvent(rdpInput *, uint16_t, uint16_t, uint16_t);
@@ -42,7 +60,19 @@ private:
 #endif
     friend BOOL inputUnicodeKeyboardEvent(rdpInput *, uint16_t, uint16_t);
 
+    /**
+     * Called when the state of modifier keys changes.
+     *
+     * \param flags The state of modifier keys.
+     */
     bool synchronizeEvent(uint32_t flags);
+    /**
+     * Called when a new mouse event was sent.
+     *
+     * \param x The X position, in client coordinates.
+     * \param y The Y position, in client coordinates.
+     * \param flags Mouse button state and other flags.
+     */
     bool mouseEvent(uint16_t x, uint16_t y, uint16_t flags);
     bool extendedMouseEvent(uint16_t x, uint16_t y, uint16_t flags);
     bool keyboardEvent(uint16_t code, uint16_t flags);
