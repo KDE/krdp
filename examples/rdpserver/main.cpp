b6f29d6b804782045808c68ce173ed1d10352365
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-04-26 15:06:48 +0200

    Add command line parameters for RDP server settings

--- examples/rdpserver/main.cpp
+++ examples/rdpserver/main.cpp
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
+#include <QCommandLineParser>
 #include <QDebug>
 #include <QGuiApplication>
 
@@ -15,13 +16,47 @@
 int main(int argc, char **argv)
 {
     QGuiApplication application{argc, argv};
+    application.setApplicationName(u"krdp-server"_qs);
+    application.setApplicationDisplayName(u"KRDP Example Server"_qs);
+
+    QCommandLineParser parser;
+    parser.addOptions({
+        {{u"u"_qs, u"username"_qs}, u"The username to use for login. Required."_qs, u"username"_qs},
+        {{u"p"_qs, u"password"_qs}, u"The password to use for login. Required."_qs, u"password"_qs},
+        {u"port"_qs, u"The port to use for connections. Defaults to 3389."_qs, u"port"_qs, u"3389"_qs},
+        {u"certificate"_qs, u"The TLS certificate file to use. Defaults to 'server.crt' in the current directory."_qs, u"certificate"_qs, u"server.crt"_qs},
+        {u"certificate-key"_qs,
+         u"The TLS certificate key to use. Defaults to 'server.key' in the current directory."_qs,
+         u"certificate-key"_qs,
+         u"server.key"_qs},
+    });
+    parser.addHelpOption();
+    parser.process(application);
+
+    if (!parser.isSet(u"username"_qs) || !parser.isSet(u"password"_qs)) {
+        qCritical() << "A username and password needs to be provided.";
+        parser.showHelp(1);
+    }
+
+    auto certificate = std::filesystem::path(parser.value(u"certificate"_qs).toStdString());
+    auto certificateKey = std::filesystem::path(parser.value(u"certificate-key"_qs).toStdString());
+
+    if (!std::filesystem::exists(certificate)) {
+        qCritical() << "The specified certificate file" << certificate.string() << "does not exist. A valid TLS certificate file is required.";
+        parser.showHelp(2);
+    }
+
+    if (!std::filesystem::exists(certificateKey)) {
+        qCritical() << "The specified certificate key" << certificateKey.string() << "does not exist. A valid TLS certificate key is required.";
+        parser.showHelp(3);
+    }
 
     KRdp::Server server;
 
     server.setAddress(QHostAddress::Any);
-    server.setPort(3389);
-    server.setUserName(QStringLiteral("test"));
-    server.setPassword(QStringLiteral("test"));
+    server.setPort(parser.value(u"port"_qs).toInt());
+    server.setUserName(parser.value(u"username"_qs));
+    server.setPassword(parser.value(u"password"_qs));
     server.setTlsCertificate(std::filesystem::path("server.crt"));
     server.setTlsCertificateKey(std::filesystem::path("server.key"));
 
@@ -54,7 +89,7 @@ int main(int argc, char **argv)
     });
 
     if (!server.start()) {
-        return 1;
+        return -1;
     } else {
         return application.exec();
     }
