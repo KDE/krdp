939de8404e92ce144cc40b489335dd996a207946
Akseli Lahtinen <akselmo@akselmo.dev>
2024-04-04 18:53:17 +0300

    Rename things

--- src/service/krdpservice.cpp
+++ src/service/krdpservice.cpp
@@ -7,19 +7,25 @@
 #include "krdpservice.h"
 
 #include <kpluginfactory.h>
+#include <qdbusconnection.h>
+#include <qdbusinterface.h>
 
 K_PLUGIN_CLASS_WITH_JSON(KRDPService, "krdpservice.json");
 
-static const QString s_dbusServiceName = QStringLiteral("org.kde.plasma.krdp_service");
+static const QString s_dbusServiceName = QStringLiteral("org.kde.plasma.krdpservice");
 
 KRDPService::KRDPService(QObject *parent, const QList<QVariant> &)
     : KDEDModule(parent)
 {
     setModuleName(QStringLiteral("KRDPService"));
+    QDBusConnection bus = QDBusConnection::sessionBus();
+    bus.registerService(s_dbusServiceName);
+    qDebug() << " Starting krdpservice";
 }
 
 KRDPService::~KRDPService()
 {
+    qDebug() << " Stopping krdpservice";
 }
 
 #include "krdpservice.moc"
