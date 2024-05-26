eb3a4db549421448c85944120a34556a3d99e56b
Akseli Lahtinen <akselmo@akselmo.dev>
2024-05-27 01:19:38 +0300

    fix typo in server/Messages.sh

--- server/Messages.sh
+++ server/Messages.sh
@@ -3,4 +3,4 @@
 # SPDX-License-Identifier: CC0-1.0
 
 $EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -o $podir/krdpserver.pot
+$XGETTEXT `find . -name "*.cpp"` -o $podir/krdpserver.pot
