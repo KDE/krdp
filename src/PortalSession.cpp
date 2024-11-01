332834b1ecbd2ce70744330d635fe0c9f371cd20
David Edmundson <kde@davidedmundson.co.uk>
2024-11-01 10:09:50 +0000

    Use single config file for krdpserver
    The kcfg file for krdp server is "krdpserver" the app name is
    krdp-server.
    
    This led to all settings being in krdpserver except the restore token
    which blindly used KSharedConfig.
    
    A compatibility path for reading is left in place that can be dropped
    later.
    
    BUG: 495564

--- src/PortalSession.cpp
+++ src/PortalSession.cpp
@@ -196,8 +196,16 @@ void PortalSession::onCreateSession(uint code, const QVariantMap &result)
         {QStringLiteral("handle_token"), createHandleToken()},
         {QStringLiteral("persist_mode"), PermissionsPersistUntilExplicitlyRevoked},
     };
-    KConfigGroup restorationGroup = KSharedConfig::openConfig()->group(QStringLiteral("General"));
-    const QString restoreToken = restorationGroup.readEntry(QStringLiteral("restorationToken"));
+    KConfigGroup restorationGroup = KSharedConfig::openConfig(QStringLiteral("krdpserverrc"))->group(QStringLiteral("General"));
+    QString restoreToken = restorationGroup.readEntry(QStringLiteral("restorationToken"));
+
+    // this is a compatibility path for krdp < 6.3 that used a different name
+    // in 6.4 onwards it can be killed
+    if (restoreToken.isEmpty()) {
+        KConfigGroup restorationGroup = KSharedConfig::openConfig(QStringLiteral("krdp-serverrc"))->group(QStringLiteral("General"));
+        restoreToken = restorationGroup.readEntry(QStringLiteral("restorationToken"));
+    } // end compat
+
     if (!restoreToken.isEmpty()) {
         parameters[QStringLiteral("restore_token")] = restoreToken;
     }
@@ -252,7 +260,7 @@ void KRdp::PortalSession::onSessionStarted(uint code, const QVariantMap &result)
         return;
     }
 
-    KConfigGroup restorationGroup = KSharedConfig::openConfig()->group(QStringLiteral("General"));
+    KConfigGroup restorationGroup = KSharedConfig::openConfig(QStringLiteral("krdpserverrc"))->group(QStringLiteral("General"));
     restorationGroup.writeEntry("restorationToken", result.value(QStringLiteral("restore_token")));
 
     const auto streams = qdbus_cast<QList<PortalSessionStream>>(result.value(QStringLiteral("streams")));
