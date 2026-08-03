7464b622fb0360487a67b41d407db39e4f5aba9d
David Edmundson <kde@davidedmundson.co.uk>
2026-08-03 13:52:27 +0100

    example: Port to pipewire node API

--- examples/streamer/main.cpp
+++ examples/streamer/main.cpp
@@ -55,6 +55,7 @@ int main(int argc, char **argv)
 
     QObject::connect(&session, &KRdp::PortalSession::started, &session, [&session, &encodedStream]() {
         encodedStream.setObjectSerial(session.objectSerial());
+        encodedStream.setNodeId(session.nodeId());
         auto fd = session.takePipeWireFd();
         if (fd >= 0) {
             encodedStream.setFd(fd);
